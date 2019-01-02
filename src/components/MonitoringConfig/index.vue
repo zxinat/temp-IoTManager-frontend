194c145c1573d3cecee9f38e3bd18afb67ba5041
CoCoManYY <1344483030@qq.com>
2019-01-02 19:09:46 +0800

    [feat]监控配置

--- src/components/MonitoringConfig/index.vue
+++ src/components/MonitoringConfig/index.vue
@@ -1,17 +1,52 @@
 <template>
-    <div>
+    <div class="monitoring-config-container">
+        <h3>{{this.name}}</h3>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">设备概况</el-menu-item>
+          <el-menu-item index="2">设备状态</el-menu-item>
+          <el-menu-item index="3">设备属性</el-menu-item>
+          <el-menu-item index="4">数据统计</el-menu-item>
+          <el-menu-item index="5">报警记录</el-menu-item>
+          <el-menu-item index="6">维保记录</el-menu-item>
+        </el-menu>
+        <div v-if="currentPage==='1'"><monitoring-device-card></monitoring-device-card></div>
+        <div v-if="currentPage==='2'">2</div>
+        <div v-if="currentPage==='3'">3</div>
+        <div v-if="currentPage==='4'">4</div>
+        <div v-if="currentPage==='5'">5</div>
+        <div v-if="currentPage==='6'">6</div>
         <MonitoringMap></MonitoringMap>
     </div>
 </template>
 
 <script>
     import MonitoringMap from "../MonitoringMap/index";
+    import MonitoringDeviceCard from "../MonitoringDeviceCard/index";
     export default {
-        name: "MonitoringConfig",
-      components: {MonitoringMap}
+      name: "MonitoringConfig",
+      data(){
+        return {
+          name:'拉丝设备',
+          activeIndex: '1',
+          currentPage: '1',
+        }
+      },
+      methods:{
+        handleSelect(key, keyPath) {
+          this.currentPage=key;
+          console.log('currentpage',typeof this.currentPage);
+        }
+      },
+      components: {MonitoringDeviceCard, MonitoringMap}
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.monitoring-config-container{
+    h3{
+      display: inline;
+      font-family: "Adobe 黑体 Std R";
+      color: #007fff;
+    }
+}
 </style>
---
+++ src/components/MonitoringDeviceCard/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="monitoring-device-card-container">
+    <el-row>
+      <el-col :span="8" offset="1">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <img src="../../assets/img/chilunzu.svg">
+            <span>属性信息</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div  class="text item">
+            {{'设备名称：'+deviceName}}
+          </div>
+          <div  class="text item">
+            {{'设备编号：'+hardwareDeviceID}}
+          </div>
+          <div  class="text item">
+            {{'设备类型：'+deviceType}}
+          </div>
+          <div  class="text item">
+            {{'东经：    '+deviceName}}
+          </div>
+          <div  class="text item">
+            {{'北纬：    '+deviceName}}
+          </div>
+          <div  class="text item">
+            {{'设备状态：'+deviceState}}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8" offset="5">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="../../assets/img/cat.jpg" class="image">
+          <div style="padding: 14px;">
+            <span>设备快照</span>
+            <div class="bottom clearfix">
+              <time class="time">{{updateTime}}</time>
+              <!--<el-button class="button">操作按钮</el-button>-->
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MonitoringDeviceCard",
+    data(){
+      return {
+        "id": "1",
+        "hardwareDeviceID": "001",
+        "deviceName": "设备1",
+        "city": "上海",
+        "factory": "某工厂",
+        "workshop": "某车间",
+        "deviceState": "正常",
+        "LastConnectionTime": "2018-9-9",
+        "imageUrl": "",
+        "gatewayID": "T001",
+        "mac": "",
+        "deviceType": "类型1",
+        "createTime": "2018-1-1",
+        "updateTime": "2818-9-10",
+        "remark": "描述描述"
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .monitoring-device-card-container {
+    padding: 2%;
+    .text {
+      font-size: 14px;
+    }
+    .item {
+      margin-bottom: 18px;
+    }
+    .box-card{
+      height: 100%;
+    }
+    .time {
+      font-size: 13px;
+      color: #999;
+    }
+
+    .bottom {
+      margin-top: 13px;
+      line-height: 12px;
+    }
+
+    .button {
+      padding: 0;
+      float: right;
+    }
+
+    .image {
+      padding: 1%;
+      width: 100%;
+      display: block;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+      display: table;
+      content: "";
+    }
+
+    .clearfix:after {
+      clear: both
+    }
+  }
+</style>
