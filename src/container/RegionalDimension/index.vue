f03bd939235dc9515dc9013d5996c8545aee946a
CoCoManYY <1344483030@qq.com>
2019-06-23 02:31:34 +0800

    [feat]导出excel，报表中心部分

--- src/container/RegionalDimension/index.vue
+++ src/container/RegionalDimension/index.vue
@@ -8,9 +8,9 @@
         <div class="head-container">
           <!--选择车间-->
           <!--<el-cascader-->
-            <!--:options="nameOptions"-->
-            <!--v-model="selectedOptions"-->
-            <!--@change="handleChange">-->
+          <!--:options="nameOptions"-->
+          <!--v-model="selectedOptions"-->
+          <!--@change="handleChange">-->
           <!--</el-cascader>-->
           <span class="demonstration">选择时间</span>
           <el-date-picker
@@ -23,8 +23,10 @@
           </el-date-picker>
         </div>
         <el-row>
-          <div class="report-statistic-daily-histogram">
-          </div>
+          <el-col :span="23">
+            <div class="report-statistic-daily-histogram">
+            </div>
+          </el-col>
         </el-row>
       </el-col>
     </el-row>
@@ -38,6 +40,8 @@
 
 <script>
   import echarts from 'echarts';
+  import FileSaver from 'file-saver'
+  import XLSX from 'xlsx'
 
   export default {
     name: "RegionalDimension",
@@ -178,30 +182,87 @@
         // 时间戳数组
         selectedDate: '',
         histogramOption: {
-          legend: {},
-          tooltip: {},
-          dataset: {
-            source: [
-              ['product', '平均在线时间', '告警次数', '设备数量'],
-              ['车间1', 43.3, 85.8, 93.7],
-              ['车间2', 83.1, 73.4, 55.1],
-              ['车间3', 86.4, 65.2, 82.5],
-              ['车间4', 72.4, 53.9, 39.1],
-              ['车间5', 72.4, 53.9, 39.1],
-              ['车间6', 72.4, 53.9, 39.1],
-              ['车间7', 72.4, 53.9, 39.1],
-              ['车间8', 72.4, 53.9, 39.1],
-            ]
+          xAxis: {
+            type: 'category',
+            data: ['车间1', '车间2', '车间3', '车间4', '车间5', '车间6', '车间7']
           },
-          xAxis: {type: 'category'},
-          yAxis: {},
-          // Declare several bar series, each will be mapped
-          // to a column of dataset.source by default.
-          series: [
-            {type: 'bar',barWidth : 20},
-            {type: 'bar',barWidth : 20},
-            {type: 'bar',barWidth : 20}
-          ]
+          legend: {
+            show: true,
+            data: ['平均在线时间', '告警次数', '设备数量']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            name: '平均在线时间',
+            data: [43.3, 83.1, 86.4, 72.4, 72.40, 110, 130],
+            type: 'bar',
+            barWidth: 20
+          }, {
+            name: '告警次数',
+            data: [85.8, 73.4, 65.2, 53.9, 70, 11, 130],
+            type: 'bar',
+            barWidth: 20
+          }, {
+            name: '设备数量',
+            data: [93.7, 55.1, 82.5, 39.1, 70, 120, 10],
+            type: 'bar',
+            barWidth: 20
+          }],
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {
+                show: true,
+                title: '数据视图',
+                //readOnly: true, //设置只读，会隐藏刷新按钮。
+                lang: ['数据视图', '关闭', '导出Excel'],
+                contentToOption: function (opts) {
+                  /* generate workbook object from table */
+                  var wb = XLSX.utils.table_to_book(document.querySelector('#tableExcel_Day'))
+                  /* get binary string as output */
+                  var wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: true, type: 'array'})
+                  try {
+                    FileSaver.saveAs(new Blob([wbout], {type: 'application/octet-stream'}), '网关设备.xlsx')
+                  } catch (e) {
+                    if (typeof console !== 'undefined') console.log(e, wbout)
+                  }
+                  return wbout
+                },
+                optionToContent: function (opt) {
+                  // console.log(opt);
+
+                  var axisData = opt.xAxis[0].data; //坐标数据
+                  var series = opt.series; //折线图数据
+                  var tdHeads = '<td  style="padding: 0 10px">车间</td>'; //表头第一列
+                  var tdBodys = ''; //表数据
+                  //组装表头
+                  var nameData = new Array('平均在线时间', '告警次数', '设备数量');
+                  for (var i = 0; i < nameData.length; i++) {
+                    tdHeads += '<td style="padding: 0 10px">' + nameData[i] + '</td>';
+                  }
+                  var table = '<table id="tableExcel_Day" border="1" class="table-bordered table-striped" style="width:80%;text-align:center;" ><tbody><tr>' + tdHeads + ' </tr>';
+                  //组装表数据
+                  for (let i = 0, l = axisData.length; i < l; i++) {
+                    for (let j = 0; j < series.length; j++) {
+                      var temp = series[j].data[i];
+                      if (temp != null && temp != undefined) {
+                        tdBodys += '<td>' + temp.toFixed(2) + '</td>';
+                      } else {
+                        tdBodys += '<td></td>';
+                      }
+                    }
+                    table += '<tr><td style="padding: 0 10px">' + axisData[i] + '</td>' + tdBodys + '</tr>';
+                    tdBodys = '';
+                  }
+                  table += '</tbody></table>';
+                  // console.log(table);
+                  return table;
+                }
+              },
+              saveAsImage : {show: true}
+            }
+          },
         },
         // 左边栏数据
         treeData: [{
@@ -272,7 +333,7 @@
       // let pieChartData = await getReportStaticDaithlyPieChart(this.selectedType, this.selectedSource, this.time);
       // this.pieChartOption.series[0].data = pieChartData.data.d;
       this.initTypeChart();
-      this.initSubClassChart();
+      // this.initSubClassChart();
     },
     methods: {
       initTypeChart() {
@@ -280,11 +341,11 @@
         // 把配置和数据放这里
         this.chart.setOption(this.histogramOption)
       },
-      initSubClassChart() {
-        this.chart = echarts.init(document.getElementsByClassName('report-statistic-daily-piechart')[0]);
-        // 把配置和数据放这里
-        this.chart.setOption(this.pieChartOption)
-      },
+      // initSubClassChart() {
+      //   this.chart = echarts.init(document.getElementsByClassName('report-statistic-daily-piechart')[0]);
+      //   // 把配置和数据放这里
+      //   this.chart.setOption(this.pieChartOption)
+      // },
     },
   }
 </script>
--- src/container/TimeDimension/index.vue
+++ src/container/TimeDimension/index.vue
@@ -29,6 +29,8 @@
 
 <script>
   import echarts from 'echarts';
+  import FileSaver from 'file-saver'
+  import XLSX from 'xlsx'
 
   export default {
     name: "TimeDimension",
@@ -169,44 +171,167 @@
         // 时间戳数组
         selectedDate: '',
         histogramOption: {
-          legend: {},
-          tooltip: {},
-          dataset: {
-            source: [
-              ['product', '平均在线时间', '告警次数', '设备数量'],
-              ['2014', 72.4, 53.9, 39.1],
-              ['2015', 72.4, 53.9, 39.1],
-              ['2016', 72.4, 53.9, 39.1],
-              ['2017', 43.3, 85.8, 93.7],
-              ['2018', 83.1, 73.4, 55.1],
-              ['2019', 86.4, 65.2, 82.5],
-            ]
+          xAxis: {
+            type: 'category',
+            data: ['2014', '2015', '2016', '2017', '2018', '2019']
           },
-          xAxis: {type: 'category'},
-          yAxis: {},
-          // Declare several bar series, each will be mapped
-          // to a column of dataset.source by default.
-          series: [
-            {type: 'bar',barWidth : 30},
-            {type: 'bar',barWidth : 30},
-            {type: 'bar',barWidth : 30}
-          ]
+          legend: {
+            show: true,
+            data: ['平均在线时间', '告警次数', '设备数量']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            name: '平均在线时间',
+            data: [43.3, 83.1, 86.4, 72.4, 72.40, 110],
+            type: 'bar',
+            barWidth: 20
+          }, {
+            name: '告警次数',
+            data: [85.8, 73.4, 65.2, 53.9, 70, 11],
+            type: 'bar',
+            barWidth: 20
+          }, {
+            name: '设备数量',
+            data: [93.7, 55.1, 82.5, 39.1, 70, 20],
+            type: 'bar',
+            barWidth: 20
+          }],
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {
+                show: true,
+                title: '数据视图',
+                //readOnly: true, //设置只读，会隐藏刷新按钮。
+                lang: ['数据视图', '关闭', '导出Excel'],
+                contentToOption: function (opts) {
+                  /* generate workbook object from table */
+                  var wb = XLSX.utils.table_to_book(document.querySelector('#report-statistic-daily-table'))
+                  /* get binary string as output */
+                  var wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: true, type: 'array'})
+                  try {
+                    FileSaver.saveAs(new Blob([wbout], {type: 'application/octet-stream'}), '按年维度.xlsx')
+                  } catch (e) {
+                    if (typeof console !== 'undefined') console.log(e, wbout)
+                  }
+                  return wbout
+                },
+                optionToContent: function (opt) {
+                  // console.log(opt);
+
+                  var axisData = opt.xAxis[0].data; //坐标数据
+                  var series = opt.series; //折线图数据
+                  var tdHeads = '<td  style="padding: 0 10px">车间</td>'; //表头第一列
+                  var tdBodys = ''; //表数据
+                  //组装表头
+                  var nameData = new Array('平均在线时间', '告警次数', '设备数量');
+                  for (var i = 0; i < nameData.length; i++) {
+                    tdHeads += '<td style="padding: 0 10px">' + nameData[i] + '</td>';
+                  }
+                  var table = '<table id="report-statistic-daily-table" border="1" class="table-bordered table-striped" style="width:80%;text-align:center" ><tbody><tr>' + tdHeads + ' </tr>';
+                  //组装表数据
+                  for (let i = 0, l = axisData.length; i < l; i++) {
+                    for (let j = 0; j < series.length; j++) {
+                      var temp = series[j].data[i];
+                      if (temp != null && temp != undefined) {
+                        tdBodys += '<td>' + temp.toFixed(2) + '</td>';
+                      } else {
+                        tdBodys += '<td></td>';
+                      }
+                    }
+                    table += '<tr><td style="padding: 0 10px">' + axisData[i] + '</td>' + tdBodys + '</tr>';
+                    tdBodys = '';
+                  }
+                  table += '</tbody></table>';
+                  // console.log(table);
+                  return table;
+                }
+              },
+              saveAsImage : {show: true}
+            }
+          },
         },
         histogramOption2: {
-          legend: {},
-          tooltip: {},
-          dataset: {
-            source: []
+          xAxis: {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月','7月','8月','9月','10月','11月','12月'],
           },
-          xAxis: {type: 'category'},
-          yAxis: {},
-          // Declare several bar series, each will be mapped
-          // to a column of dataset.source by default.
-          series: [
-            {type: 'bar'},
-            {type: 'bar'},
-            {type: 'bar'}
-          ]
+          legend: {
+            show: true,
+            data: ['平均在线时间', '告警次数', '设备数量']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            name: '平均在线时间',
+            data: [43.3, 83.1, 86.4, 72.4, 72.40, 110,43.3, 83.1, 86.4, 72.4, 72.40, 110],
+            type: 'bar',
+          }, {
+            name: '告警次数',
+            data: [85.8, 73.4, 65.2, 53.9, 70, 11,85.8, 73.4, 65.2, 53.9, 70, 11],
+            type: 'bar',
+          }, {
+            name: '设备数量',
+            data: [93.7, 55.1, 82.5, 39.1, 70, 20,93.7, 55.1, 82.5, 39.1, 70, 20],
+            type: 'bar',
+          }],
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {
+                show: true,
+                title: '数据视图',
+                //readOnly: true, //设置只读，会隐藏刷新按钮。
+                lang: ['数据视图', '关闭', '导出Excel'],
+                contentToOption: function (opts) {
+                  /* generate workbook object from table */
+                  var wb = XLSX.utils.table_to_book(document.querySelector('#report-statistic-daily-table2'))
+                  /* get binary string as output */
+                  var wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: true, type: 'array'})
+                  try {
+                    FileSaver.saveAs(new Blob([wbout], {type: 'application/octet-stream'}), '按月维度.xlsx')
+                  } catch (e) {
+                    if (typeof console !== 'undefined') console.log(e, wbout)
+                  }
+                  return wbout
+                },
+                optionToContent: function (opt) {
+                  // console.log(opt);
+
+                  var axisData = opt.xAxis[0].data; //坐标数据
+                  var series = opt.series; //折线图数据
+                  var tdHeads = '<td  style="padding: 0 10px">车间</td>'; //表头第一列
+                  var tdBodys = ''; //表数据
+                  //组装表头
+                  var nameData = new Array('平均在线时间', '告警次数', '设备数量');
+                  for (var i = 0; i < nameData.length; i++) {
+                    tdHeads += '<td style="padding: 0 10px">' + nameData[i] + '</td>';
+                  }
+                  var table = '<table id="report-statistic-daily-table2" border="1" class="table-bordered table-striped" style="width:80%;text-align:center" ><tbody><tr>' + tdHeads + ' </tr>';
+                  //组装表数据
+                  for (let i = 0, l = axisData.length; i < l; i++) {
+                    for (let j = 0; j < series.length; j++) {
+                      var temp = series[j].data[i];
+                      if (temp != null && temp != undefined) {
+                        tdBodys += '<td>' + temp.toFixed(2) + '</td>';
+                      } else {
+                        tdBodys += '<td></td>';
+                      }
+                    }
+                    table += '<tr><td style="padding: 0 10px">' + axisData[i] + '</td>' + tdBodys + '</tr>';
+                    tdBodys = '';
+                  }
+                  table += '</tbody></table>';
+                  // console.log(table);
+                  return table;
+                }
+              },
+              saveAsImage : {show: true}
+            }
+          },
         },
         // 左边栏数据
         treeData: [{
@@ -289,21 +414,10 @@
         this.chart.on('click', function (params) {
           vm.showMonth = true;
           // 控制台打印数据的名称
-          vm.histogramOption2.dataset.source = [
-            ['product', '平均在线时间', '告警次数', '设备数量'],
-            ['1月', 72.4, 53.9, 39.1],
-            ['2月', 72.4, 53.9, 39.1],
-            ['3月', 72.4, 53.9, 39.1],
-            ['4月', 43.3, 85.8, 93.7],
-            ['5月', 83.1, 73.4, 55.1],
-            ['6月', 86.4, 65.2, 82.5],
-            ['7月', 72.4, 53.9, 39.1],
-            ['8月', 72.4, 53.9, 39.1],
-            ['9月', 72.4, 53.9, 39.1],
-            ['10月', 43.3, 85.8, 93.7],
-            ['11月', 83.1, 73.4, 55.1],
-            ['12月', 86.4, 65.2, 82.5],
-          ];
+          vm.histogramOption2.series[0].data= [43.3, 83.1, 86.4, 72.4, 72.40, 110,43.3, 83.1, 86.4, 72.4, 72.40, 110];
+          vm.histogramOption2.series[1].data= [85.8, 73.4, 65.2, 53.9, 70, 11,85.8, 73.4, 65.2, 53.9, 70, 11];
+          vm.histogramOption2.series[2].data=  [93.7, 55.1, 82.5, 39.1, 70, 20,93.7, 55.1, 82.5, 39.1, 70, 20];
+
           // 异步f防止bug
           vm.$nextTick(() => {
             vm.initSubClassChart();
