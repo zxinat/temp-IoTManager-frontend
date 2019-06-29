0d5a6f51e9bb934691057f2b72e0a497d2abcc93
CoCoManYY <1344483030@qq.com>
2019-06-30 02:22:35 +0800

    [feat]device data (dynamic|static)

--- src/components/DataStatistic/index.vue
+++ src/components/DataStatistic/index.vue
@@ -36,7 +36,7 @@
         timeDuration: [new Date() - 86400000, new Date()],
         // 折线图数据
         chart: {},
-        chartOption: {
+        dynamicChartOption: {
           tooltip: {
             trigger: 'axis',
             formatter: function (params) {
@@ -69,32 +69,110 @@
             data: []
           }]
         },
+        chartOption:{
+            // color:[ '#d14a61', '#675bba'],
+            xAxis: {
+              type : 'category',
+              data :  function (){
+                var list = [];
+                for (var i = 1; i <= 12; i++) {
+                  list.push('11-' + i);
+                }
+                return list;
+              }()
+            },
+            yAxis: {
+              type : 'value'
+            },
+            tooltip : {
+              trigger: 'axis',
+              axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : ''        // 默认为直线，可选为：'line' | 'shadow'
+              },
+              formatter: function (params) {
+                var min = params[0];
+                var max = params[1];
+                var ave = params[2];
+                return min.name + '<br/>' + min.seriesName + ' : ' + min.value + '<br/>' + max.seriesName + ' : ' +(max.value+min.value) + '<br/>' + ave.seriesName + ' : ' + ave.value;
+              }
+            },
+            series: [
+              {
+                name: '最小值',
+                type: 'bar',
+                stack:  '总量',
+                itemStyle: {
+                  normal: {
+                    barBorderColor: 'rgba(0,0,0,0)',
+                    color: 'rgba(0,5,99,0)'
+                  }
+                },
+                data: [800, 1700, 1400, 1200, 300, 0,800, 1700, 1400, 1200, 300, 0]
+              },
+              {
+                name: '最大值',
+                type: 'bar',
+                stack: '总量',
+                data:[2900, 900, 300, 200, 900, 300,2900, 900, 300, 200, 900, 300]
+              },
+              {
+                name: '平均值',
+                type: 'line',
+                // stack: '总量',
+                label: {
+                  normal: {
+                    show: true,
+                  }
+                },
+                data:[1900, 1800, 1300, 1400, 500, 30,1900, 1800, 1300, 1400, 500, 30]
+              }
+            ]
+        },
         now: +new Date(),
-        value: Math.random() * 1000
+        value: Math.random() * 1000,
+        tempSetInterval:null
       }
     },
     methods: {
       initChart() {
+        // 获取动态数据接口
         for (let i = 0; i < 1000; i++) {
-          console.log(this.getData());
-          this.chartOption.series[0].data.push(this.getData());
+          this.dynamicChartOption.series[0].data.push(this.getData());
         }
         this.chart = echarts.init(document.getElementsByClassName('data-statistic-line-chart')[0]);
         // // 把配置和数据放这里
-        this.chart.setOption(this.chartOption)
+        this.chart.setOption(this.dynamicChartOption)
+        this.polling()
+      },
+      initDynamicChart() {
+        this.stopPolling();
+        // 获取实时数据接口
+        this.chart.setOption(this.dynamicChartOption,true);
+        this.polling()
       },
       setTimeDuration(value) {
         switch (value) {
           case 'day':
             this.timeDuration=[new Date() - 86400000, new Date()];
+            this.stopPolling();
+            // 数据修改操作接口，传入快捷时间方式，返回min数组、max数组、ave数组、x轴数组
+            this.chart.setOption(this.chartOption,true);
             break;
           case 'week':
+            this.stopPolling();
             this.timeDuration=[new Date() - 86400000 * 7, new Date()];
+            // 数据修改操作接口，传入快捷时间方式，返回min数组、max数组、ave数组、x轴数组
+            this.chart.setOption(this.chartOption,true);
             break;
           case 'month':
+            this.stopPolling();
             this.timeDuration=[new Date() - 86400000 * 30, new Date()];
+            // 数据修改操作接口，传入快捷时间方式，返回min数组、max数组、ave数组、x轴数组
+            this.chart.setOption(this.chartOption,true);
             break;
           default:
+            // 数据修改操作接口，实时数据
+            this.initDynamicChart();
             break;
         }
       },
@@ -112,22 +190,25 @@
       },
       polling() { // 添加轮询接口
         let vm = this;
-        setInterval(function () {
+        this.tempSetInterval=setInterval(function () {
           for (let i = 0; i < 5; i++) {
-            vm.chartOption.series[0].data.shift();
-            vm.chartOption.series[0].data.push(vm.getData());
+            vm.dynamicChartOption.series[0].data.shift();
+            vm.dynamicChartOption.series[0].data.push(vm.getData());
           }
           vm.chart.setOption({
             series: [{
-              data: vm.chartOption.series[0].data
+              data: vm.dynamicChartOption.series[0].data
             }]
           });
         }, 1000);
+      },
+      stopPolling(){
+        let vm=this;
+        clearInterval(vm.tempSetInterval);
       }
     },
     mounted() {
       this.initChart();
-      this.polling()
     }
   }
 </script>
