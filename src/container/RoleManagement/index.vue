a24c575e23df70c46c78bf14614556516579ffd4
JackySong59 <1055335350@qq.com>
2019-07-17 12:45:11 +0800

    Fixed update user page

--- src/container/RoleManagement/index.vue
+++ src/container/RoleManagement/index.vue
@@ -12,16 +12,16 @@
           </div>
         </el-col>
         <!--<el-col :span="4">-->
-          <!--<div class="grid-content bg-purple-dark">-->
-            <!--<el-select v-model="department" :placeholder="$t('message.allDepartment')">-->
-              <!--<el-option-->
-                <!--v-for="item in departmentOptions"-->
-                <!--:key="item.value"-->
-                <!--:label="item.label"-->
-                <!--:value="item.label">-->
-              <!--</el-option>-->
-            <!--</el-select>-->
-          <!--</div>-->
+        <!--<div class="grid-content bg-purple-dark">-->
+        <!--<el-select v-model="department" :placeholder="$t('message.allDepartment')">-->
+        <!--<el-option-->
+        <!--v-for="item in departmentOptions"-->
+        <!--:key="item.value"-->
+        <!--:label="item.label"-->
+        <!--:value="item.label">-->
+        <!--</el-option>-->
+        <!--</el-select>-->
+        <!--</div>-->
         <!--</el-col>-->
         <el-col :span="4">
           <div class="grid-content bg-purple-dark">
@@ -56,10 +56,10 @@
           prop="phoneNumber"
           label="电话">
         </el-table-column>
-        <el-table-column
-          prop="department"
-          label="部门">
-        </el-table-column>
+        <!--<el-table-column-->
+        <!--prop="department"-->
+        <!--label="部门">-->
+        <!--</el-table-column>-->
         <!--TODO:权限  -->
         <el-table-column
           label="操作">
@@ -73,27 +73,27 @@
     </div>
 
     <el-dialog title="修改用户" :visible.sync="dialogFormVisible">
-      <el-tag
-        :key="tag"
-        v-for="tag in dynamicTags"
-        closable
-        :disable-transitions="false"
-        @close="handleClose(tag)">
-        {{tag}}
-      </el-tag>
-      <el-select v-model="inputValue"
-                 v-if="inputVisible"
-                 ref="saveTagInput"
-                 @keyup.enter.native="handleInputConfirm"
-                 placeholder="请选择">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.label">
-        </el-option>
-      </el-select>
-      <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+      <!--<el-tag-->
+      <!--:key="tag"-->
+      <!--v-for="tag in dynamicTags"-->
+      <!--closable-->
+      <!--:disable-transitions="false"-->
+      <!--@close="handleClose(tag)">-->
+      <!--{{tag}}-->
+      <!--</el-tag>-->
+      <!--<el-select v-model="inputValue"-->
+                 <!--v-if="inputVisible"-->
+                 <!--ref="saveTagInput"-->
+                 <!--@keyup.enter.native="handleInputConfirm"-->
+                 <!--placeholder="请选择">-->
+        <!--<el-option-->
+          <!--v-for="item in options"-->
+          <!--:key="item.value"-->
+          <!--:label="item.label"-->
+          <!--:value="item.label">-->
+        <!--</el-option>-->
+      <!--</el-select>-->
+      <!--<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>-->
       <el-form :model="editedUser">
         <el-form-item label="用户名">
           <el-input v-model="editedUser.userName"></el-input>
@@ -107,16 +107,16 @@
         <el-form-item label="电话">
           <el-input v-model="editedUser.phoneNumber"></el-input>
         </el-form-item>
-        <el-form-item label="部门">
-          <el-select v-model="editedUser.department" placeholder="请选择部门">
-            <el-option
-              v-for="item in departmentOptions"
-              :key="item.id"
-              :label="item.departmentName"
-              :value="item.departmentName">
-            </el-option>
-          </el-select>
-        </el-form-item>
+        <!--<el-form-item label="部门">-->
+        <!--<el-select v-model="editedUser.department" placeholder="请选择部门">-->
+        <!--<el-option-->
+        <!--v-for="item in departmentOptions"-->
+        <!--:key="item.id"-->
+        <!--:label="item.departmentName"-->
+        <!--:value="item.departmentName">-->
+        <!--</el-option>-->
+        <!--</el-select>-->
+        <!--</el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
@@ -143,38 +143,38 @@
         <el-form-item label="电话" prop="phoneNumber" label-width="120px">
           <el-input v-model="newUser.phoneNumber"></el-input>
         </el-form-item>
-        <el-form-item label="部门" prop="department" label-width="120px">
-          <el-select v-model="newUser.department" placeholder="请选择部门">
-            <el-option
-              v-for="item in departmentOptions"
-              :key="item.id"
-              :label="item.departmentName"
-              :value="item.departmentName">
-            </el-option>
-          </el-select>
-        </el-form-item>
+        <!--<el-form-item label="部门" prop="department" label-width="120px">-->
+        <!--<el-select v-model="newUser.department" placeholder="请选择部门">-->
+        <!--<el-option-->
+        <!--v-for="item in departmentOptions"-->
+        <!--:key="item.id"-->
+        <!--:label="item.departmentName"-->
+        <!--:value="item.departmentName">-->
+        <!--</el-option>-->
+        <!--</el-select>-->
+        <!--</el-form-item>-->
         <!--<el-form-item label="权限" label-width="120px">-->
-          <!--<el-tag-->
-            <!--:key="tag"-->
-            <!--v-for="tag in newUser.authorities"-->
-            <!--closable-->
-            <!--:disable-transitions="false"-->
-            <!--@close="handleClose2(tag)">-->
-            <!--{{tag}}-->
-          <!--</el-tag>-->
-          <!--<el-select v-model="inputValue"-->
-                     <!--v-if="inputVisible"-->
-                     <!--ref="saveTagInput"-->
-                     <!--@keyup.enter.native="handleInputConfirm2"-->
-                     <!--placeholder="请选择">-->
-            <!--<el-option-->
-              <!--v-for="item in options"-->
-              <!--:key="item.value"-->
-              <!--:label="item.label"-->
-              <!--:value="item.label">-->
-            <!--</el-option>-->
-          <!--</el-select>-->
-          <!--<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>-->
+        <!--<el-tag-->
+        <!--:key="tag"-->
+        <!--v-for="tag in newUser.authorities"-->
+        <!--closable-->
+        <!--:disable-transitions="false"-->
+        <!--@close="handleClose2(tag)">-->
+        <!--{{tag}}-->
+        <!--</el-tag>-->
+        <!--<el-select v-model="inputValue"-->
+        <!--v-if="inputVisible"-->
+        <!--ref="saveTagInput"-->
+        <!--@keyup.enter.native="handleInputConfirm2"-->
+        <!--placeholder="请选择">-->
+        <!--<el-option-->
+        <!--v-for="item in options"-->
+        <!--:key="item.value"-->
+        <!--:label="item.label"-->
+        <!--:value="item.label">-->
+        <!--</el-option>-->
+        <!--</el-select>-->
+        <!--<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>-->
         <!--</el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -197,12 +197,12 @@
   } from '../../api/api';
   import {isvalidPhone} from '../../common/validate';
 
-  var validPhone=(rule, value,callback)=>{
-    if (!value){
+  var validPhone = (rule, value, callback) => {
+    if (!value) {
       callback(new Error('请输入电话号码'))
-    }else  if (!isvalidPhone(value)){
+    } else if (!isvalidPhone(value)) {
       callback(new Error('请输入正确的11位手机号码'))
-    }else {
+    } else {
       callback()
     }
   }
@@ -222,75 +222,75 @@
       return {
         rules: {
           userName: [
-            { required: true, message: '名称必填', trigger: 'blur' }
+            {required: true, message: '名称必填', trigger: 'blur'}
           ],
           displayName: [
-            { required: true, message: '昵称必填', trigger: 'blur' }
+            {required: true, message: '昵称必填', trigger: 'blur'}
           ],
           password: [
-            {required: true, message: '密码必填', trigger: 'blur' }
+            {required: true, message: '密码必填', trigger: 'blur'}
           ],
           passwordConfirm: [
-            { required: true, validator: validatePass2, trigger: 'blur' }
+            {required: true, validator: validatePass2, trigger: 'blur'}
           ],
           email: [
-            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-            { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+            {required: true, message: '请输入邮箱地址', trigger: 'blur'},
+            {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
           ],
           phoneNumber: [
-            { required: true,  trigger: 'blur' , validator: validPhone}
+            {required: true, trigger: 'blur', validator: validPhone}
           ],
           department: [
-            { required: true, message: '请填写部门', trigger: 'blur' }
+            {required: true, message: '请填写部门', trigger: 'blur'}
           ]
         },
         username: '',
         departmentOptions: [],
         department: '',
         tableData: [],
-        currentID:'',
+        currentID: '',
         dialogFormVisible: false,
-        createNewDialogFormVisible:false,
+        createNewDialogFormVisible: false,
         dynamicTags: [],
         inputVisible: false,
         inputValue: '',
         options: [],
-        newUser:{
-          userName:'',
-          displayName:'',
-          password:'',
-          passwordConfirm:'',
-          email:'',
+        newUser: {
+          userName: '',
+          displayName: '',
+          password: '',
+          passwordConfirm: '',
+          email: '',
           phoneNumber: '',
-          department:''
+          department: ''
           // authorities:[]
         },
         editedUser: {
-          userName:'',
-          displayName:'',
-          password:'',
-          email:'',
+          userName: '',
+          displayName: '',
+          password: '',
+          email: '',
           phoneNumber: '',
-          department:''
+          department: ''
         }
       }
     },
     methods: {
       async searchDeviceByName() {
         let result = (await getUserTable(this.username, this.department));
-        if(result.data.c===200){
+        if (result.data.c === 200) {
           this.$message({
             message: '查找成功',
             type: 'success'
           });
           this.tableData = result.data.d;
-        }else{
+        } else {
           this.$message.error('查找失败');
         }
       },
       async editAuthority(row) {
         this.dialogFormVisible = true;
-        this.currentID=row.id;
+        this.currentID = row.id;
         this.editedUser = (await getUserById(this.currentID)).data.d;
         //this.dynamicTags = (await getAuthorities(row.id)).data.d;
       },
@@ -299,18 +299,19 @@
       async deleteUser(row) {
         try {
           this.$confirm('确认删除？')
-            .then(async _=> {
-              if((await deleteUser(row.id)).data.c===200){
+            .then(async _ => {
+              if ((await deleteUser(row.id)).data.c === 200) {
                 this.$message({
                   message: '删除成功',
                   type: 'success'
                 });
                 this.tableData = (await getUserTable()).data.d;
-              }else{
+              } else {
                 this.$message.error('删除失败');
               }
             })
-            .catch(_ => {});
+            .catch(_ => {
+            });
         } catch (e) {
           console.log(e)
         }
@@ -318,34 +319,34 @@
       handleClose(tag) {
         this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
       },
-      handleClose2(tag){
+      handleClose2(tag) {
         this.newUser.authorities.splice(this.newUser.authorities.indexOf(tag), 1);
       },
       showInput() {
         this.inputVisible = true;
       },
-    async confirmEdit(row) {
-      if((await editUser(this.currentID, this.editedUser)).data.c===200){
-        this.$message({
-          message: '更新成功',
-          type: 'success'
-        });
-        this.tableData = (await getUserTable()).data.d;
-      }else{
-        this.$message.error('更新失败');
-      }
-      this.dialogFormVisible = false;
-    },
-      async confirmCreateNew(){
+      async confirmEdit(row) {
+        if ((await editUser(this.currentID, this.editedUser)).data.c === 200) {
+          this.$message({
+            message: '更新成功',
+            type: 'success'
+          });
+          this.tableData = (await getUserTable()).data.d;
+        } else {
+          this.$message.error('更新失败');
+        }
+        this.dialogFormVisible = false;
+      },
+      async confirmCreateNew() {
         console.log(this.newUser);
-        if((await createNewUser(this.newUser)).data.c===200){
+        if ((await createNewUser(this.newUser)).data.c === 200) {
           this.$message({
             message: '添加成功',
             type: 'success'
           });
           //更新下面表格数据
           this.tableData = (await getUserTable()).data.d;
-        }else{
+        } else {
           this.$message.error('添加失败');
         }
         this.createNewDialogFormVisible = false
@@ -367,8 +368,13 @@
         this.inputVisible = false;
         this.inputValue = '';
       },
-      async searchUser(){
-        this.tableData = (await searchUsers(this.username)).data.d;
+      async searchUser() {
+        console.log(this.username);
+        if (this.username !== "") {
+          this.tableData = (await searchUsers(this.username)).data.d;
+        } else {
+          this.tableData = (await getUserTable()).data.d;
+        }
       }
     },
     async mounted() {
